d740941afc8358194a751727a2c0109eaa4fa0e1
Philipesko <philipesko@yandex.ru>
2021-10-10 12:29:26 +0300

    Add class vkapi. Add to gitignore exception for creds.

--- CoursePythonProject/main.py
+++ CoursePythonProject/main.py
@@ -1,3 +1,11 @@
+from _typeshed import Self
 import requests
 import os
+from NetologyOOP.CoursePythonProject.credentials import TOKEN_VK
 
+
+class Vkapi:
+    def __init__(self, profile_id):
+        self.profile = profile_id
+        self.token = TOKEN_VK
+        
