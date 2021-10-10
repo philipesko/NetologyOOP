64a7db70c3e789f25389380e83220758f9d5f719
Philipesko <philipesko@yandex.ru>
2021-10-10 19:36:11 +0300

    Try to vkapi

--- CoursePythonProject/main.py
+++ CoursePythonProject/main.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import time
+import vk
+import pprint
 from tqdm import tqdm
 
 
@@ -9,12 +11,17 @@ from credentials import TOKEN_VK
 
 
 class Vkapi:
-    def __init__(self, profile_id):
+    def __init__(self, profile_id='https://vk.com/begemot_korovin'):
         self.profile = profile_id
         self.token = TOKEN_VK
         
     def result(self):
-        return self.token
+        #user_info = vk.method('users.get', {'slug': 'begemot_korovin',
+                  #'fields': 'nickname', 'name_case': 'nom'})
+
+        user_info = requests.get(
+            f'https://api.vk.com/method/users.get?slug=begemot_korovin&fields=bdate&access_token={self.token}&v=5.131').json()
+        return user_info['response'][0]['id']
 
 
 
@@ -23,10 +30,9 @@ if __name__ == "__main__":
     pbar = tqdm(total=3, desc="VKAPI")
     pbar.update(1)
     time.sleep(1)
-    vkapi.result()
+    print(vkapi.result())
     pbar.update(1)
     time.sleep(1)
-    #print(vkapi.result())
     time.sleep(1)
     pbar.update(1)
         
